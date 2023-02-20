cb0c1a6cdba6300d2195d54b97b544ac8c473742
aditya191201 <deshpande.adu01@gmail.com>
2023-02-20 12:08:06 +0530

    links

--- src/Navbar/MyLinks.jsx
+++ src/Navbar/MyLinks.jsx
@@ -2,36 +2,7 @@ export const links = [{name: 'Home', mainlink:"/home"},
     {name: 'Products', mainlink:"/productcategory", 
     },
     {name: 'Solutions',mainlink:"/solutioncategory", submenu:true, sublinks:[
-        {
-            Head: "Meeting and Presentation",
-            sublink:[
-                { name:'Boardrooms and Conference Rooms'},
-                { name: 'Interactive Training Rooms'}
-            ]
-        },
-        {
-            Head: "Specialised AV Solutions",
-            sublink:[
-                { name:'Auditorium'},
-                { name:'Operations Centres'}
-                
-            ]
-        },
-        {
-            Head: "Meeting and Presentation",
-            sublink:[
-                { name:'Boardrooms and Conference Rooms'},
-                { name: 'Interactive Training Rooms'}
-            ]
-        },
-        {
-            Head: "Specialised AV Solutions",
-            sublink:[
-                { name:'Auditorium'},
-                { name:'Operations Centres'}
-                
-            ]
-        },
+        
         {
             Head: "Meeting and Presentation",
             sublink:[
