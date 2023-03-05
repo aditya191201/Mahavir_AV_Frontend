af1ffafbe6a5375fcc96e7faf4ebbce0eb6ab447
aditya191201 <deshpande.adu01@gmail.com>
2023-03-05 19:48:41 +0530

    url change

--- src/Url.jsx
+++ src/Url.jsx
@@ -1,5 +1,5 @@
 // var url = "https://brewingjava.co.in"
-var url = "http://localhost:8080"
-// var url = "http://159.89.175.173:8080"
+// var url = "http://localhost:8080"
+var url = "http://159.89.175.173:8080"
 
 export default url;
